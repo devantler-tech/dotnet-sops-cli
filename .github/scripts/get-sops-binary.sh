da081f97ec3a48a061ec9914496db902b25e7347
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-10-17 07:13:33 +0200

    Refactor download function in get-sops-binary.sh to support zip archives

--- .github/scripts/get-sops-binary.sh
+++ .github/scripts/get-sops-binary.sh
@@ -6,13 +6,18 @@ get() {
   local binary=$2
   local target_dir=$3
   local target_name=$4
-  local isTar=$5
+  local archiveType=$5
 
   echo "Downloading $target_name from $url"
-  if [ "$isTar" = true ]; then
+  if [ "$archiveType" = "tar" ]; then
     curl -LJ "$url" | tar xvz -C "$target_dir" "$binary"
     mv "$target_dir/$binary" "${target_dir}/$target_name"
-  elif [ "$isTar" = false ]; then
+  elif [ "$archiveType" = "zip" ]; then
+    curl -LJ "$url" -o "$target_dir/$target_name.zip"
+    unzip "$target_dir/$target_name.zip" -d "$target_dir"
+    mv "$target_dir/$binary" "${target_dir}/$target_name"
+    rm "$target_dir/$target_name.zip"
+  elif [ "$archiveType" = false ]; then
     curl -LJ "$url" -o "$target_dir/$target_name"
   fi
   chmod +x "$target_dir/$target_name"
@@ -22,4 +27,4 @@ get "https://getbin.io/getsops/sops?os=darwin&arch=amd64" "sops" "Devantler.SOPS
 get "https://getbin.io/getsops/sops?os=darwin&arch=arm64" "sops" "Devantler.SOPSCLI/assets/binaries" "sops-darwin-arm64" false
 get "https://getbin.io/getsops/sops?os=linux&arch=amd64" "sops" "Devantler.SOPSCLI/assets/binaries" "sops-linux-amd64" false
 get "https://getbin.io/getsops/sops?os=linux&arch=arm64" "sops" "Devantler.SOPSCLI/assets/binaries" "sops-linux-arm64" false
-curl -s "https://api.github.com/repos/getsops/sops/releases" | grep "browser_download.*.exe" | cut -d '"' -f 4 | sort -V| tail -n 2 | head -1 | xargs curl -LJ -o Devantler.SOPSCLI/assets/binaries/sops-windows-amd64.exe
+get "https://getbin.io/getsops/sops?os=windows&arch=amd64" "sops.exe" "Devantler.SOPSCLI/assets/binaries" "sops-windows-amd64.exe" false
