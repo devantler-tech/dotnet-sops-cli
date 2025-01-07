395d3f8a724c247206ecf8182775c276d9484f72
Nikolai Emil Damm <neq@energinet.dk>
2025-01-07 21:29:42 +0100

    Rework sops cli
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

---
+++ Devantler.SOPSCLI.Tests/SOPSTests/RunAsyncTests.cs
@@ -0,0 +1,24 @@
+using CliWrap;
+
+namespace Devantler.SOPSCLI.Tests.SOPSTests;
+
+/// <summary>
+/// Tests for the <see cref="SOPS.RunAsync(string[], CommandResultValidation, bool, bool, CancellationToken)" /> method.
+/// </summary>
+public class RunAsyncTests
+{
+  /// <summary>
+  /// Tests that the binary can return the version of the sops CLI command.
+  /// </summary>
+  /// <returns></returns>
+  [Fact]
+  public async Task RunAsync_Version_ReturnsVersion()
+  {
+    // Act
+    var (exitCode, message) = await SOPS.RunAsync(["--version"]);
+
+    // Assert
+    Assert.Equal(0, exitCode);
+    Assert.Matches(@"^sops \d+\.\d+\.\d+$", message.Trim());
+  }
+}
