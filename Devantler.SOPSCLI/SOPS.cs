5ba40cead98b77a306b8a5ba25291b6f6e5156d2
Nikolai Emil Damm <ned@devantler.com>
2025-02-26 20:44:48 +0100

    fix:Set executable permissions for non-Windows platforms (#117)
    * fix: improve error handling and set executable permissions for non-Windows platforms
    
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>
    
    * refactor: remove SOPSException class as it is no longer needed
    
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>
    
    ---------
    
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- Devantler.SOPSCLI/SOPS.cs
+++ Devantler.SOPSCLI/SOPS.cs
@@ -27,9 +27,15 @@ public static class SOPS
       _ => throw new PlatformNotSupportedException($"Unsupported platform: {Environment.OSVersion.Platform} {RuntimeInformation.ProcessArchitecture}"),
     };
     string binaryPath = Path.Combine(AppContext.BaseDirectory, binary);
-    return !File.Exists(binaryPath) ?
-      throw new SOPSException($"{binaryPath} not found.") :
-      Cli.Wrap(binaryPath);
+    if (!File.Exists(binaryPath))
+    {
+      throw new FileNotFoundException($"{binaryPath} not found.");
+    }
+    if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    {
+      File.SetUnixFileMode(binaryPath, UnixFileMode.UserExecute | UnixFileMode.GroupExecute | UnixFileMode.OtherExecute);
+    }
+    return Cli.Wrap(binaryPath);
   }
 
   /// <summary>
