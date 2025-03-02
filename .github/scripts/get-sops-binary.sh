db7ddf167cf4786f9311fcc433ee66afad258200
Nikolai Emil Damm <neq@energinet.dk>
2025-03-02 01:07:30 +0100

    chore: update paths to sops binaries in get-sops-binary.sh script
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- .github/scripts/get-sops-binary.sh
+++ .github/scripts/get-sops-binary.sh
@@ -23,9 +23,9 @@ get() {
   chmod +x "$target_dir/$target_name"
 }
 
-get "https://getbin.io/getsops/sops?os=darwin&arch=amd64" "sops" "Devantler.SOPSCLI/runtimes/osx-x64/native" "sops-osx-x64" false
-get "https://getbin.io/getsops/sops?os=darwin&arch=arm64" "sops" "Devantler.SOPSCLI/runtimes/osx-arm64/native" "sops-osx-arm64" false
-get "https://getbin.io/getsops/sops?os=linux&arch=amd64" "sops" "Devantler.SOPSCLI/runtimes/linux-x64/native" "sops-linux-x64" false
-get "https://getbin.io/getsops/sops?os=linux&arch=arm64" "sops" "Devantler.SOPSCLI/runtimes/linux-arm64/native" "sops-linux-arm64" false
+get "https://getbin.io/getsops/sops?os=darwin&arch=amd64" "sops" "src/Devantler.SOPSCLI/runtimes/osx-x64/native" "sops-osx-x64" false
+get "https://getbin.io/getsops/sops?os=darwin&arch=arm64" "sops" "src/Devantler.SOPSCLI/runtimes/osx-arm64/native" "sops-osx-arm64" false
+get "https://getbin.io/getsops/sops?os=linux&arch=amd64" "sops" "src/Devantler.SOPSCLI/runtimes/linux-x64/native" "sops-linux-x64" false
+get "https://getbin.io/getsops/sops?os=linux&arch=arm64" "sops" "src/Devantler.SOPSCLI/runtimes/linux-arm64/native" "sops-linux-arm64" false
 version=$(curl -s https://api.github.com/repos/getsops/sops/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-get "https://github.com/getsops/sops/releases/download/$version/sops-$version.exe" "sops.exe" "Devantler.SOPSCLI/runtimes/win-x64/native" "sops-win-x64.exe" false
+get "https://github.com/getsops/sops/releases/download/$version/sops-$version.exe" "sops.exe" "src/Devantler.SOPSCLI/runtimes/win-x64/native" "sops-win-x64.exe" false
