f4c2edf34b936c74c2e98023c3845ad4c4b026c7
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-03-31 21:58:05 +0200

    fix: add support for Windows arm64 binary in SOPS command retrieval
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- src/Devantler.SOPSCLI/SOPS.cs
+++ src/Devantler.SOPSCLI/SOPS.cs
@@ -25,6 +25,7 @@ public static class SOPS
       (PlatformID.Unix, Architecture.X64, "linux-x64") => "sops-linux-x64",
       (PlatformID.Unix, Architecture.Arm64, "linux-arm64") => "sops-linux-arm64",
       (PlatformID.Win32NT, Architecture.X64, "win-x64") => "sops-win-x64.exe",
+      (PlatformID.Win32NT, Architecture.Arm64, "win-arm64") => "sops-win-arm64.exe",
       _ => throw new PlatformNotSupportedException($"Unsupported platform: {Environment.OSVersion.Platform} {RuntimeInformation.ProcessArchitecture}"),
     };
     string binaryPath = Path.Combine(AppContext.BaseDirectory, binary);
